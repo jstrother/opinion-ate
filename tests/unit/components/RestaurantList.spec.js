7f705b730e865e3066482fff7ed0d6d1dbf88e4a
Jim Strother <jim.strother@gmail.com>
2021-06-06 16:02:32 -0500

    RestaurantList.spec.js passing -- all green

--- tests/unit/components/RestaurantList.spec.js
+++ tests/unit/components/RestaurantList.spec.js
@@ -24,4 +24,35 @@ describe('RestaurantList', () => {
 
     expect(restaurantModule.actions.load).toHaveBeenCalled();
   });
+
+  it('displays the restaurants', () => {
+    const records = [
+      { id: 644, name: 'Pasta Place' },
+      { id: 645, name: 'Salad Place' },
+    ];
+
+    const restaurantModule = {
+      namespaced: true,
+      state: { records },
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+
+    const store = new Vuex.Store({
+      modules: {
+        restaurants: restaurantModule,
+      },
+    });
+
+    const wrapper = mount(RestaurantList, { localVue, store });
+
+    const firstRestaurantName = wrapper.findAll('[data-testid="restaurant"]').at(0).text();
+
+    expect(firstRestaurantName).toBe('Pasta Place');
+
+    const secondRestaurantName = wrapper.findAll('[data-testid="restaurant"]').at(1).text();
+
+    expect(secondRestaurantName).toBe('Salad Place');
+  });
 });
