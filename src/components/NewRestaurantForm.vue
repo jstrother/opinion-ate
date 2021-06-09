68460af021b62f7bd9637756a918ee80880b5860
Jim Strother <jim.strother@gmail.com>
2021-06-08 19:31:09 -0500

    Add: new e2e and unit tests e2e test for creating a new restaurant unit test for NewRestaurantForm component:

---
+++ src/components/NewRestaurantForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="handleSave">
+    <v-text-field
+      placeholder="Add Restaurant"
+      filled
+      type="text"
+      v-model="name"
+      data-testid="new-restaurant-name-field"
+    />
+    <v-btn
+      type="submit"
+      color="primary"
+      class="black--text"
+      data-testid="new-restaurant-submit-button"
+    >
+      Add
+    </v-btn>
+  </form>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data() {
+    return {
+      name: '',
+    };
+  },
+  name: 'NewRestaurantForm',
+  methods: {
+    ...mapActions({
+      createRestaurant: 'restaurants/create',
+    }),
+    handleSave() {
+      this.createRestaurant(this.name);
+    },
+  },
+};
+</script>
--- src/components/RestaurantScreen.vue
+++ src/components/RestaurantScreen.vue
@@ -2,6 +2,7 @@
   <v-card>
     <v-card-title>Restaurants</v-card-title>
     <v-card-text>
+      <NewRestaurantForm />
       <RestaurantList />
     </v-card-text>
   </v-card>
@@ -9,11 +10,13 @@
 
 <script>
 import RestaurantList from '@/components/RestaurantList';
+import NewRestaurantForm from '@/components/NewRestaurantForm';
 
 export default {
   name: 'RestaurantScreen',
   components: {
     RestaurantList,
+    NewRestaurantForm,
   },
 };
 </script>
